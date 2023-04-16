62817f320ea67903dc5dff73d777d50f46830123
Marcin Blichowski <74534746+mblichowski@users.noreply.github.com>
2023-04-16 14:14:57 +0200

    dynamic module path fixed

--- src/ChartJsInterop.cs
+++ src/ChartJsInterop.cs
@@ -3,32 +3,32 @@ using System.Threading.Tasks;
 
 namespace PSC.Blazor.Components.Chartjs
 {
-    public class ChartJsInterop
-    {
-        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+	public class ChartJsInterop
+	{
+		private readonly Lazy<Task<IJSObjectReference>> moduleTask;
 
-        public ChartJsInterop(IJSRuntime jsRuntime)
-        {
-            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import",
-                "./_content/PSC.Blazor.Components.Chartjs/Chart.js").AsTask());
-        }
+		public ChartJsInterop(IJSRuntime jsRuntime)
+		{
+			moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import",
+				"./_content/PSC.Blazor.Components.Chartjs-extended/Chart.js").AsTask());
+		}
 
-        public async ValueTask Setup(DotNetObjectReference<IChartConfig> dotNetObjectRef, IChartConfig Config)
-        {
-            var module = await moduleTask.Value;
-            await module.InvokeVoidAsync("chartSetup", Config.CanvasId, dotNetObjectRef, Config);
-        }
+		public async ValueTask Setup(DotNetObjectReference<IChartConfig> dotNetObjectRef, IChartConfig Config)
+		{
+			var module = await moduleTask.Value;
+			await module.InvokeVoidAsync("chartSetup", Config.CanvasId, dotNetObjectRef, Config);
+		}
 
-        public async ValueTask AddData(string CanvasId, List<string> labels, int datasetIndex, List<decimal?> data)
-        {
-            var module = await moduleTask.Value;
-            await module.InvokeVoidAsync("addData", CanvasId, labels, datasetIndex, data);
-        }
+		public async ValueTask AddData(string CanvasId, List<string> labels, int datasetIndex, List<decimal?> data)
+		{
+			var module = await moduleTask.Value;
+			await module.InvokeVoidAsync("addData", CanvasId, labels, datasetIndex, data);
+		}
 
-        public async ValueTask AddNewDataset<T>(string CanvasId, T dataset) where T : class
-        {
-            var module = await moduleTask.Value;
-            await module.InvokeVoidAsync("addNewDataset", CanvasId, dataset);
-        }
-    }
+		public async ValueTask AddNewDataset<T>(string CanvasId, T dataset) where T : class
+		{
+			var module = await moduleTask.Value;
+			await module.InvokeVoidAsync("addNewDataset", CanvasId, dataset);
+		}
+	}
 }
